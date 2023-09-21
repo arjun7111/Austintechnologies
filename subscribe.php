d6f6a62a608fd05cc12634bf9e6b250de1c9614a
arjun7111 <144689088+arjun7111@users.noreply.github.com>
2023-09-21 18:58:46 +0530

    Initial commit

---
+++ subscribe.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Subscribe to Email Updates</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Subscribe to Email Updates</h1>
+        <form action="subscribe.php" method="POST">
+            <label for="email">Email:</label>
+            <input type="email" id="7111arjunkumar@gmail.com" name="Arjun" required>
+            <button type="submit">Subscribe</button>
+        </form>
+    </div>
+</body>
+</html>
